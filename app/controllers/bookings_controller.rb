b02fa3e5e90f2cbc4dd889e886aa309289615b70
Elisa <codersquirrelbln@gmail.com>
2019-08-20 17:39:14 +0200

    add bookings_create_method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,22 @@
 class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new(bookings_params)
+    if @booking.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
   def destroy
     @booking = Booking.find(params[:id])
     @booking.destroy
     redirect_to dashboard_path
+  end
+
+  private
+
+  def bookings_params
+    params.require(:booking).permit(:start_date, :end_date, :beach_id, :user_id)
   end
 end
