c0c060ea9f6a552aa92939de5df12febcffcb9ae
Elisa <codersquirrelbln@gmail.com>
2019-08-20 15:02:32 +0200

    add bookings_controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  # resources :beaches do
+  #   resources :bookings
+  # end
   root to: 'pages#home'
+  get 'dashboard', to: 'pages#dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
