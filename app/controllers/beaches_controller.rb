f42ef6074d41b110a23db0603d54efe40eac9c77
Ronan Recune <r.recune@gmail.com>
2019-08-21 16:03:31 +0200

    day 3

--- app/controllers/beaches_controller.rb
+++ app/controllers/beaches_controller.rb
@@ -1,5 +1,13 @@
 class BeachesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
   def index
     @beaches = Beach.all
   end
+
+ def show
+    @beach = Beach.find(params[:id])
+ end
+
 end
+
